d6b55cdee6c88e242dec16cbeaad89e1f5da047a
henriksih <henrik.sihvonen@gmail.com>
2025-10-29 17:01:21 +0100

    Added UserInfoWindow and ViewModel

---
+++ CookMaster/ViewModels/UserInfoWindowViewModel.cs
@@ -0,0 +1,50 @@
+﻿using CookMaster.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CookMaster.ViewModels
+{
+    internal class UserInfoWindowViewModel
+    {
+        public UserManager? UserManager { get; }
+
+        private string _username;
+        private string _password;
+        private string _country;
+        private List<string> _countries;
+        private string _error;
+
+        public string Username
+        {
+            get => _username;
+            set { _username = value; OnPropertyChanged(); CommandManager.InvalidateRequerySuggested(); }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set { _password = value; OnPropertyChanged(); CommandManager.InvalidateRequerySuggested(); }
+        }
+
+        public string Country
+        {
+            get => _country;
+            set { _country = value; OnPropertyChanged(); CommandManager.InvalidateRequerySuggested(); }
+        }
+
+        public List<string> Countries
+        {
+            get => _countries;
+            set
+            {
+                _countries = value;
+                OnPropertyChanged();
+            }
+        }
+
+    }
+}
